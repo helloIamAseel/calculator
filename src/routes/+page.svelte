3eec6c48bf436d447f4d1438ca5cb3d5c8d69015
Aseel <aseel4leesa@gmail.com>
2024-06-24 23:24:56 +0300

    most buttons are now functional

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,51 +3,72 @@
       A Calculator
    </title>
  </svelte:head>
+<script lang="ts"> 
+  
+  let equation: string ="";
 
- <div class=" bg-gray-600 rounded-3xl grid grid-cols-4 gap-2 p-6 pt-7
+  function addToEquation(value: string){
+     equation += value;
+  }
+
+
+</script>
+
+
+
+ <div class=" bg-slate-700 rounded-3xl grid grid-cols-4 gap-2 p-6 pt-7
   shadow-slate-900 shadow-2xl">
 
     <div class="bg-amber-500 rounded-full col-span-4 h-20
      px-4 pt-5 mb-3 text-white text-4xl font-bold
       shadow-slate-700 shadow-md">
-    114
+    
+      {equation}
 
     </div>
 
-      <button class="bg-white font-extrabold"> % </button>
-      <button class="bg-white pl-4"> <!--square root icon-->
+    <button class="bg-white font-extrabold active:bg-gray-400">
+       % </button>
+    <button class="bg-white pl-4  active:bg-gray-400"> 
+        <!--square root icon-->
         <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24">
             <path fill="currentColor" d="M15.382 4H22v2h-5.382L9 21.236L5.382 14H2v-2h4.618L9 16.764z"/>
         </svg>    
     </button>
-      <button class="bg-white"> CE </button>
-      <button class="bg-blue-500 text-white"> C </button>
+      <button class="bg-white  active:bg-gray-400"> CE </button>
+      <button class="bg-blue-500 text-white active:bg-blue-700"> 
+       C </button>
 
-      <button class="bg-white"> sin </button>
-      <button class="bg-white"> cos </button>
-      <button class="bg-white"> tan </button>
-      <button class="bg-lime-500 text-white text-3xl"> ^ </button>
+      <button class="bg-white active:bg-gray-400"> sin </button>
+      <button class="bg-white active:bg-gray-400"> cos </button>
+      <button class="bg-white active:bg-gray-400"> tan </button>
+      <button on:click={() => addToEquation('^')} class="bg-lime-500 active:bg-lime-700 text-white text-3xl">
+        ^ </button>
 
-      <button> 7 </button>
-      <button> 8 </button>
-      <button> 9 </button>
-      <button class="bg-red-500 text-white text-3xl"> − </button>
+      <button on:click={() => addToEquation('7')}> 7 </button>
+      <button on:click={() => addToEquation('8')}> 8 </button>
+      <button on:click={() => addToEquation('9')}> 9 </button>
+      <button on:click={() => addToEquation('−')} class="bg-red-500 active:bg-red-700 text-white text-3xl"> 
+        − </button>
 
-      <button> 4 </button>
-      <button> 5 </button>
-      <button> 6 </button>
-      <button class="bg-sky-500 text-white text-3xl"> ÷ </button>
+      <button on:click={() => addToEquation('4')}> 4 </button>
+      <button on:click={() => addToEquation('5')}> 5 </button>
+      <button on:click={() => addToEquation('6')}> 6 </button>
+      <button on:click={() => addToEquation('÷')} class="bg-sky-500 active:bg-sky-700 text-white text-3xl">
+         ÷ </button>
 
-      <button> 1 </button>
-      <button> 2 </button>
-      <button> 3 </button>
-      <button class="bg-yellow-500 text-white text-3xl"> × </button>
+      <button on:click={() => addToEquation('1')}> 1 </button>
+      <button on:click={() => addToEquation('2')}> 2 </button>
+      <button on:click={() => addToEquation('3')}> 3 </button>
+      <button on:click={() => addToEquation('×')} class="bg-yellow-500 active:bg-yellow-700 text-white text-3xl">
+        × </button>
       
 
       <button> . </button>
-      <button> 0 </button>
-      <button class="bg-white"> = </button>
-      <button class="bg-green-500 text-white text-3xl"> + </button>
+      <button on:click={() => addToEquation('0')}> 0 </button>
+      <button class="bg-white active:bg-gray-400"> = </button>
+      <button on:click={() => addToEquation('+')} class="bg-green-500 active:bg-green-700 text-white text-3xl">
+        + </button>
 </div>
 
 
