3ea4a9005e0bc98c0e532b9d23604ff65aa11835
Aseel <aseel4leesa@gmail.com>
2024-06-24 16:29:05 +0300

    early structure for the calc with colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,17 +4,34 @@
    </title>
  </svelte:head>
 
- <div class="bg-red-200 h-[30rem] w-[20rem] rounded-3xl flex justify-center items-center">
-    
-   <div class="bg-blue-300 h-[5rem] w-[5rem] rounded-full">
+ <div class="bg-gray-700 rounded-3xl grid grid-cols-4">
+      <button class="bg-slate-100"> % </button>
+      <button class="bg-slate-100"> √ </button>
+      <button class="bg-slate-100"> CE </button>
+      <button class="bg-blue-600 text-white"> C </button>
 
+      <button> 7 </button>
+      <button> 8 </button>
+      <button> 9 </button>
+      <button class="bg-red-500"> − </button>
 
+      <button> 4 </button>
+      <button> 5 </button>
+      <button> 6 </button>
+      <button class="bg-sky-500"> ÷ </button>
 
-   </div>
+      <button> 1 </button>
+      <button> 2 </button>
+      <button> 3 </button>
+      <button class="bg-amber-400"> × </button>
 
-
-
+      <button> . </button>
+      <button> 0 </button>
+      <button> = </button>
+      <button class="bg-green-500"> + </button>
 </div>
+
+
 
  
 
